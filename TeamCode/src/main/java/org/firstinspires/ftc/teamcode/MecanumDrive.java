15e2610282f8963092c9a36582cdd6dba64d76d9
IAmZeMuffinMan <mepekse@gmail.com>
2024-10-28 16:19:38 -0600

    Commmiting because Logan asked me to

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -68,8 +68,8 @@ public final class MecanumDrive {
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 5.76649685547509;
+        public double kV = 0.004356115051411358;
         public double kA = 0;
 
         // path profile parameters (in inches)
