d9a212aff6ad9f3511477913e5d7b010019e30af
Logan <60761520+Preloading@users.noreply.github.com>
2024-11-18 16:55:30 -0700

    Implement claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManualControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManualControl.java
@@ -16,7 +16,12 @@ public class RobotManualControl extends OpMode {
     private DcMotor motorBL = null; // Back Left
     private DcMotor motorBR = null; // Back Right
     //endregion
+
+    //region Slide
     private DcMotor motorSlide = null;
+    private DcMotor motorSwing = null;
+    private Servo clawServo = null;
+    //endregion
     //region Arm
     /**
      * Motor attached to the bot to move the arm containing the main arm mechanism.
@@ -34,15 +39,20 @@ public class RobotManualControl extends OpMode {
      * The third servo on the arm. Controls the wheel to scoop the game pieces.
      */
     private CRServo arm_Scoop = null;
+
+    private double armSmallHorizontal = 0.5;
+    private double armVertical = 0.5;
     //endregion
     //endregion
 
     // Current Drive setting
-    // This is here so I can be lazy and reuse some automode code
+    // This is here so  I can be lazy and reuse some automode code
     // TODO: Later implement this in a different class to practice DRY, and make some things easier
     private double drivePwr = 0;
     private double strafePwr = 0;
     private double yawPwr = 0;
+    private boolean isClipped = false;
+    private boolean lastControllerState = false;
 
 //region Initialization
     @Override
@@ -69,10 +79,20 @@ public class RobotManualControl extends OpMode {
 
         //endregion
 
+        //region Slide
         motorSlide = hardwareMap.get(DcMotor.class, "slideMotor");
 
         motorSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motorSwing = hardwareMap.get(DcMotor.class, "swing");
+        motorSwing.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorSwing.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        clawServo = hardwareMap.get(Servo.class, "claw");
+
+
+        //endregion
 
         //region Arm Hardware
         arm_BigHorizontal = hardwareMap.get(DcMotor.class, "bigHorizontal");
@@ -91,32 +111,35 @@ public class RobotManualControl extends OpMode {
     @Override
     public void loop() {
         //region Drive
+        double speed = 1.0;
+        if (gamepad1.right_bumper) {
+            speed = 0.5;
+        }
         MoveRobot(-gamepad1.left_stick_y, -gamepad1.left_stick_x, -gamepad1.right_stick_x);
-        moveRobotInternal();
+
         //endregion
 
         //region Arm
-        double armPower = gamepad2.left_stick_x * 0.5;
+        double armPower = gamepad2.left_stick_x * 0.2;
         arm_BigHorizontal.setPower(armPower);
 
-        double armSmallHorizontal = -gamepad2.right_stick_x + 0.5;
+        armSmallHorizontal += -gamepad2.right_stick_x * 0.01;
+        armSmallHorizontal = Math.max(-1, Math.min(1, armSmallHorizontal));
         arm_SmallHorizontal.setPosition(armSmallHorizontal);
 
-        double armVertical = gamepad2.right_stick_y + 0.5;
+        armVertical += -gamepad2.right_stick_y * 0.01;
+        armVertical = Math.max(-1, Math.min(1, armVertical));
         arm_VerticalServo.setPosition(armVertical);
 
-        double armScoop = gamepad2.left_stick_y;
+        double armScoop = (gamepad2.b ? 1 : 0) + (gamepad2.a ? -1 : 0);
         arm_Scoop.setPower(armScoop);
 
-        // Get our arm data
-
+        // Arm telemetry
         telemetry.addLine("Arm data");
         telemetry.addLine("Small Horizonal " + arm_SmallHorizontal.getPosition());
         telemetry.addLine("Vertical " + arm_VerticalServo.getPosition());
 
         telemetry.addLine("Slide Position " + motorSlide.getCurrentPosition());
-//        telemetry.addLine("Scoop " + a;
-
         //endregion
 
 
@@ -134,6 +157,24 @@ public class RobotManualControl extends OpMode {
         }
         motorSlide.setPower(slidePower);
 
+
+        // Clip
+
+        if (gamepad2.right_bumper) {
+            if (!lastControllerState) {
+                isClipped = !isClipped;
+                lastControllerState = true;
+            }
+        } else {
+            lastControllerState = false;
+        }
+
+        clawServo.setPosition(isClipped ? 1 : 0);
+
+        // Swing
+        //motorSwing.setTargetPosition(gamepad2.left_stick_y * );
+        telemetry.addLine("Swing: " + motorSwing.getCurrentPosition());
+
         //endregion
         telemetry.update();
     }
@@ -144,7 +185,7 @@ public class RobotManualControl extends OpMode {
         yawPwr = yaw;
     }
 
-    private void moveRobotInternal() {
+    private void moveRobotInternal(double speedLimit) {
         // Calculate wheel powers.
         double leftFrontPower    =   drivePwr -strafePwr -yawPwr;
         double rightFrontPower   =   drivePwr +strafePwr +yawPwr;
