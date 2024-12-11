b6cafe9fc9d5519d0fa64797be59411beb55f965
IAmZeMuffinMan <mepekse@gmail.com>
2024-12-11 09:38:34 -0700

    Made some fixes to the PrimaryAutoModeClass and made dedicated blue and red side autos to use in comp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoextend/RedSideAutoAuto.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.autoextend;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.PrimaryAutoModeClass;
+import org.firstinspires.ftc.teamcode.RedSideAuto;
+
+@Autonomous(name="Primary Auto Mode", group = "Competition Ready")
+public class RedSideAutoAuto extends RedSideAuto {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoextend/RedSideAutoTeleop.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.autoextend;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RedSideAuto;
+
+@TeleOp(name="Primary Auto Mode", group = "Competition Ready")
+public class RedSideAutoTeleop extends RedSideAuto {
+}
