54eaafa5ab887479102780ff90d4f29c88a1126e
Logan <60761520+Preloading@users.noreply.github.com>
2024-12-14 00:18:52 -0700

    refactor: RobotManualControl cleaned up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManualControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManualControl.java
@@ -9,19 +9,15 @@ public class RobotManualControl extends DeepHorOpMode {
     public double armSmallHorizontal = 0.52;
     public double armVertical = 0.57;
     //endregion
-    //endregion
-
 
     // Targets
     public double verticalTarget = -4;
-    private boolean lastControllerStateX = false;
-    private boolean altSwingMode = false;
+
+    // Held down checks
+    private boolean isBackheld = false;
 
     // Bot Config
     double deadzone = 0.1;
-
-    private boolean isBackheld = false;
-
 
     @Override
     public void runOpMode() {
@@ -32,7 +28,7 @@ public class RobotManualControl extends DeepHorOpMode {
         }
     }
 
-//region Initialization
+    //region Initialization
     public void initOpMode() {
         ConfigureHardware(true);
         if (!CrossOpModeData.isInitialized) {
@@ -42,8 +38,6 @@ public class RobotManualControl extends DeepHorOpMode {
 
         telemetry.addLine("Ready to Start");
         telemetry.update();
-
-
     }
     //endregion
 
@@ -68,64 +62,16 @@ public class RobotManualControl extends DeepHorOpMode {
         UpdateMoveRobot(speed);
         //endregion
 
+        //region Controller 2
         boolean isSlowModeActive = !gamepad2.left_bumper;
 
-        //region Arm
-        // -131 to -10
-//
-//
-//        if (gamepad2.back) {
-//            if (!lastControllerState2) {
-//                altArmMode = !altArmMode;
-//                lastControllerState2 = true;
-//            }
-//        } else {
-//            lastControllerState2 = false;
-//        }
-//
-//        if (altArmMode) {
-//            double armPower = gamepad2.left_stick_x * (isSlowModeActive ? 0.2 : 0.5);
-//            arm_BigHorizontal.setPower(armPower);
-//            if (arm_BigHorizontal.getMode() != DcMotor.RunMode.RUN_USING_ENCODER ||
-//                    arm_BigHorizontal.getZeroPowerBehavior() != DcMotor.ZeroPowerBehavior.BRAKE) {
-//
-//                arm_BigHorizontal.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//                arm_BigHorizontal.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//            }
-//        } else {
-//            if (gamepad2.left_stick_x != 0) {
-//                armTarget = (arm_BigHorizontal.getCurrentPosition() + (int) (gamepad2.left_stick_x * (isSlowModeActive ? 10 : 20)));
-//            }
-//            // Adjust target based on position limits
-//            if (armTarget <= -130) {
-//                // Prevent the motor from moving further negative
-//                armTarget = -125;
-//            } else if (armTarget >= -10) {
-//                // Prevent the motor from moving further positive
-//                armTarget = -15;
-//            }
-//
-//            if (arm_BigHorizontal.getMode() != DcMotor.RunMode.RUN_TO_POSITION ||
-//                    arm_BigHorizontal.getZeroPowerBehavior() != DcMotor.ZeroPowerBehavior.BRAKE) {
-//
-//                arm_BigHorizontal.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                arm_BigHorizontal.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//            }
-//            arm_BigHorizontal.setPower(1);
-//            arm_BigHorizontal.setTargetPosition(armTarget);
-//        }
-//
-//        armSmallHorizontal += -gamepad2.right_stick_x * (isSlowModeActive ? .01 : .03);
-//        armSmallHorizontal = Math.max(0.35, Math.min(0.85, armSmallHorizontal));
-//        arm_SmallHorizontal.setPosition(armSmallHorizontal);
-//
-//        armVertical += -gamepad2.right_stick_y * (isSlowModeActive ? .01 : .03);
-//        armVertical = Math.max(0.15, Math.min(0.75, armVertical));
-//        arm_VerticalServo.setPosition(armVertical);
+        //region Collection Arm / First Stage
 
+        // Scoop
         double armScoop = (gamepad2.b ? 1 : 0) + (gamepad2.a ? -1 : 0);
         MoveArmScoop(armScoop);
 
+        // First stage bucket
         if (gamepad2.x) {
             arm_Vertical.setTargetPosition(5);
             arm_Vertical.setPower(0.1);
@@ -135,6 +81,7 @@ public class RobotManualControl extends DeepHorOpMode {
             arm_Vertical.setPower(0.3);
         }
 
+        // First stage bucket power control
         if (arm_Vertical.getTargetPosition() == 5) {
             if (arm_Vertical.getCurrentPosition() < 30) {
                 arm_Vertical.setPower(0);
@@ -142,22 +89,14 @@ public class RobotManualControl extends DeepHorOpMode {
                 arm_Vertical.setPower(0.1);
             }
         }
+        //endregion
 
-        // auto transfer shit
-        // slide should be at -1480
+        //region Slide / Second Stage
+        // Slide
+        double slidePower = gamepad2.right_trigger - gamepad2.left_trigger;
+        MoveSlidePwr(slidePower);
 
-//        verticalTarget += ((gamepad2.dpad_down ? 1 : 0) - (gamepad2.dpad_up ? 1 : 0)) * (isSlowModeActive ? 1 : 3);
-
-//        // Adjust target based on position limits
-//        if (verticalTarget < -130) {
-//            // Prevent the motor from moving further negative
-//            verticalTarget = -129;
-//        } else if (verticalTarget > 1) {
-//            // Prevent the motor from moving further positive
-//            verticalTarget = 0;
-//        }
-////        } else {
-//        // Normal mode
+        // Second stage arm
         if (gamepad2.dpad_down) {
             verticalTarget = -11;
         } else if (gamepad2.dpad_up) {
@@ -166,9 +105,7 @@ public class RobotManualControl extends DeepHorOpMode {
         motorSwing.setTargetPosition((int) verticalTarget);
         motorSwing.setPower(0.5);
 
-        // Sync bucket position
-//        bucketTargetPosition += (gamepad2.right_stick_y * (isSlowModeActive ? 0.01 : 0.03));
-//        topBucketServo.setPosition(bucketTargetPosition);
+        // Second stage bucket
         bucketTargetPosition = -0.1;
         if (gamepad2.right_bumper) {
             bucketTargetPosition = 0.2;
@@ -177,21 +114,13 @@ public class RobotManualControl extends DeepHorOpMode {
             bucketTargetPosition = 0;
         }
 
+        // Bucket syncing (since this moves!)
         if (!isBucketBalencingEnabled) {
             StartBucketSync();
         }
-
-
-//        // Arm telemetry
-//        telemetry.addLine("Arm data");
-//        telemetry.addLine("Big Horizontal " + arm_BigHorizontal.getCurrentPosition());
-//        telemetry.addLine("Small Horizonal " + arm_SmallHorizontal.getPosition());
-//        telemetry.addLine("Vertical " + arm_VerticalServo.getPosition());
-
         //endregion
 
-
-
+        //region Arm Assist
         if (gamepad2.back) {
             if (!isBackheld) {
                 isBackheld = true;
@@ -204,45 +133,24 @@ public class RobotManualControl extends DeepHorOpMode {
         } else {
             isBackheld = false;
         }
+        //endregion
 
-        //region Slide
-        double slidePower = gamepad2.right_trigger - gamepad2.left_trigger;
-        MoveSlidePwr(slidePower);
+        //endregion
 
-        if (gamepad2.start) {
-            if (!lastControllerStateX) {
-                altSwingMode = !altSwingMode;
-                lastControllerStateX = true;
-            }
-        } else {
-            lastControllerStateX = false;
-        }
-
-//        if (altSwingMode) {
-            // Swing
-            // swing 0 to -113
-
-
-
-//
-//        motorSwing.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//        motorSwing.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-//
-//        motorSwing.setPower(1);
-//
+        //region Telemetry
         telemetry.addLine("Slide Position " + motorSlide.getCurrentPosition());
-//        telemetry.addLine("Swing Target" + swingTarget);
         telemetry.addLine("Swing Position " + motorSwing.getCurrentPosition());
 
         telemetry.addLine("Top Bucket Position (servo) " + topBucketServo.getPosition());
         telemetry.addLine("Bucket Target Position (servo)" + bucketTargetPosition);
 
-        //endregion
+
         telemetry.addLine("X Odometer: " + motorFR.getCurrentPosition());
         telemetry.addLine("Y Odometer: " + motorFL.getCurrentPosition());
         telemetry.addLine("Arm Vertical: " + arm_Vertical.getCurrentPosition());
 
         telemetry.update();
+        //endregion
     }
     //endregion
 
