e0179d1f0fc7da61f85161f4972e1ad1fa958ad6
Mitchell <mepekse@gmail.com>
2025-02-05 22:45:42 -0700

    MeepMeepHarder, Son
    Just testing some red side auto for fun, might try to make a red side auto and see how it works on friday

--- MeepMeepTestingFr/src/main/java/FtcRobotController/MeepMeepTesting/MeepMeepTestingFr.java
+++ MeepMeepTestingFr/src/main/java/FtcRobotController/MeepMeepTesting/MeepMeepTestingFr.java
@@ -1,6 +1,7 @@
 package FtcRobotController.MeepMeepTesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -17,13 +18,10 @@ public class MeepMeepTestingFr {
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
                 .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+
+                .waitSeconds(2)
+                .splineToLinearHeading(new Pose2d(40,-55,75),90)
+
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
