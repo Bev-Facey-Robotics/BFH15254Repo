d3c623d1295e0ea51ed3ca3e4c0d86256cee3c1c
Mitchell <mepekse@gmail.com>
2025-02-21 16:58:07 -0700

    Tuning Roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,13 +63,13 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.RIGHT;
 
         // drive model parameters
-        public double inPerTick = 0.001362232851002;
-        public double lateralInPerTick = 0.0011899958367416078;
+        public double inPerTick = 0.0013596963344853;
+        public double lateralInPerTick = 0.011871498535615201;
         public double trackWidthTicks =  10608.08920897234;
 
         // feedforward parameters (in tick units)
-        public double kS = 0.8699281023714232;
-        public double kV =  0.00032721823640495523;
+        public double kS = 1.0371118768172405;
+        public double kV =  0.0031428906987509116;
         public double kA = 0.00008;
 
         // path profile parameters (in inches)a
