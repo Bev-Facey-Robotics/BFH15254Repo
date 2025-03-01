dc273f7aa333a697ea589879158ea9af2497c800
Preloading <60761520+Preloading@users.noreply.github.com>
2025-02-28 17:36:56 -0700

    finish the hang extention and start work on auto hang

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/actions/AutoHang.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.actions;
+
+import org.firstinspires.ftc.teamcode.hardware.HangExtention;
+import org.firstinspires.ftc.teamcode.internal.ActionElement;
+import org.firstinspires.ftc.teamcode.internal.HardwareManager;
+
+public class AutoHang extends ActionElement {
+    @Override
+    public void run() throws InterruptedException, NullPointerException {
+        // Reserve the hardware
+        HangExtention hangExtention = (HangExtention) HardwareManager.ReserveHardware(this, "HangExtention");
+        hangExtention.MoveToPosition(0);
+    }
+
+    @Override
+    public boolean isAutoRestart() {
+        return false;
+    }
+
+    @Override
+    public int getPriority() {
+        return 10;
+    }
+}
